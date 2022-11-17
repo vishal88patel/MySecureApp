b2b7396b20697d5ad5a26d297a5b4480d41988aa
Kishan Dungarani <sunnydevkrushna@gmail.com>
2022-11-18 02:19:32 +0530

    kyc flow

--- lib/presentation/EnterPersonalDetails/enter_personal_detail_screen.dart
+++ lib/presentation/EnterPersonalDetails/enter_personal_detail_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -68,10 +70,11 @@ class EnterPersonalDetailScreen extends StatelessWidget {
                           height: getVerticalSize(43),
                         ),
                         TextFormField(
+                          keyboardType: TextInputType.number,
                           readOnly: false,
                           style: TextStyle(color: Colors.white),
                           inputFormatters: [
-                               FilteringTextInputFormatter.allow(RegExp(r'[0-9 ]+'))
+                            DateTextFormatter()
                           ],
                           decoration: InputDecoration(
                             hintText: 'DD/MM/YYYY',
@@ -121,5 +124,69 @@ class EnterPersonalDetailScreen extends StatelessWidget {
         ),
       ),
     ));
+  }
+
+}
+class DateTextFormatter extends TextInputFormatter {
+  static const _maxChars = 8;
+
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue,
+      TextEditingValue newValue,
+      ) {
+    String separator = '/';
+    var text = _format(
+      newValue.text,
+      oldValue.text,
+      separator,
+    );
+
+    return newValue.copyWith(
+      text: text,
+      selection: updateCursorPosition(
+        oldValue,
+        text,
+      ),
+    );
+  }
+
+  String _format(
+      String value,
+      String oldValue,
+      String separator,
+      ) {
+    var isErasing = value.length < oldValue.length;
+    var isComplete = value.length > _maxChars + 2;
+
+    if (!isErasing && isComplete) {
+      return oldValue;
+    }
+
+    value = value.replaceAll(separator, '');
+    final result = <String>[];
+
+    for (int i = 0; i < min(value.length, _maxChars); i++) {
+      result.add(value[i]);
+      if ((i == 1 || i == 3) && i != value.length - 1) {
+        result.add(separator);
+      }
+    }
+
+    return result.join();
+  }
+
+  TextSelection updateCursorPosition(
+      TextEditingValue oldValue,
+      String text,
+      ) {
+    var endOffset = max(
+      oldValue.text.length - oldValue.selection.end,
+      0,
+    );
+
+    var selectionEnd = text.length - endOffset;
+
+    return TextSelection.fromPosition(TextPosition(offset: selectionEnd));
   }
 }
--- lib/utils/HelperFiles/ui_utils.dart
+++ lib/utils/HelperFiles/ui_utils.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 import '../../App Configurations/color_constants.dart';
 import '../../App Configurations/image_constants.dart';
 import '../../theme/app_style.dart';
@@ -15,33 +16,12 @@ class UIUtils {
   static void showProgressDialog({isCancellable = false}) async {
     if (!isProgressVisible) {
       Get.dialog(
-        Center(
-          child: SizedBox(
-            width: 100,
-            height: 100,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Image.asset(
-                  // you can replace this with Image.asset
-                  'asset/icons/progress_icon.png',
-                  fit: BoxFit.cover,
-                  height: 80,
-                  width: 80,
-                ),
-                // you can replace
-                SizedBox(
-                  height: 150,
-                  width: 150,
-                  child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(
-                        ColorConstant.skyE8),
-                    strokeWidth: 5,
-                  ),
-                ),
-              ],
-            ),
-          ),
+        Container(
+          color: Colors.black.withOpacity(0.8),
+          height: Get.size.height / 2.5,
+          width: Get.size.width / 1.25,
+          child: Lottie.asset("asset/animations/progressbar.json",
+              height: Get.size.height / 2, width: Get.size.width),
         ),
         barrierDismissible: isCancellable,
       );
