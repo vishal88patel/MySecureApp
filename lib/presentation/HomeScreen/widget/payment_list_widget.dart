f32cdffd0808507676d48148d14721d483ef8abb
SPT-kishan <kishandob.spt@gmail.com>
2022-11-27 23:22:44 +0530

    home screen

---
+++ lib/presentation/HomeScreen/widget/payment_list_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:my_secure_app/App%20Configurations/color_constants.dart';
+import 'package:my_secure_app/theme/app_style.dart';
+import 'package:my_secure_app/utils/HelperFiles/math_utils.dart';
+
+class PaymentListWidget extends StatelessWidget {
+  final String title;
+  final String iconName;
+  const PaymentListWidget({Key? key,
+    required this.title,
+    required this.iconName}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: getVerticalSize(120), width: getHorizontalSize(90),
+    child: Column(crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            // height: getVerticalSize(60), width: getVerticalSize(60),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(16),
+              color: ColorConstant.skyF6 ),
+            child: Padding(
+              padding: const EdgeInsets.all(18),
+              child: Image.asset(iconName, height: getVerticalSize(28), width: getVerticalSize(28),
+              ),
+            ),
+          ),
+          SizedBox(height: getVerticalSize(10),),
+          Text(
+            title,
+            style: AppStyle.textStyleDMSANS.copyWith(
+                color: ColorConstant.naturalBlack,
+                fontWeight: FontWeight.w400,
+                fontSize: getFontSize(15)),
+            maxLines: 2, textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
