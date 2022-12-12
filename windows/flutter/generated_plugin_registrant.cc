68cbb7631a5ec0a4c5549967d8d61fb1ed546063
Kishan Dungarani <sunnydevkrushna@gmail.com>
2022-12-13 00:38:26 +0530

    figma design

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <connectivity_plus/connectivity_plus_windows_plugin.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
+#include <platform_device_id_windows/platform_device_id_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  ConnectivityPlusWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+  PlatformDeviceIdWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PlatformDeviceIdWindowsPlugin"));
+}
