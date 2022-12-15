70ef26adf2b37482e3ff8d6c3af24664baf3fcd2
SPT-kishan <kishandob.spt@gmail.com>
2022-12-16 01:08:00 +0530

    loan ui change

--- lib/presentation/LoanCalculator/loan_lname_screen.dart
+++ lib/presentation/LoanCalculator/loan_lname_screen.dart
@@ -20,22 +20,22 @@ class LoanLnameScreen extends StatelessWidget {
     return Scaffold(
         backgroundColor: ColorConstant.backgroundColor,
         body: SingleChildScrollView(
-            child: Container(
-                height: size.height,
-                child: Stack(
-                  children: [
-                    SafeArea(
+          child: Container(
+              height: size.height,
+              color: ColorConstant.buttonGreen.withOpacity(0.3),
+              child: Stack(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: getVerticalSize(20)),
+                    child: SafeArea(
                       child: Stack(
                         children: [
-                          Padding(
-                            padding:  EdgeInsets.symmetric(horizontal: getHorizontalSize(20)),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  height: getVerticalSize(10),
-                                ),
-                                Row(
+                          Column( crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding:  EdgeInsets.symmetric(horizontal: getHorizontalSize(20)),
+                                child: Row(
                                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                   children: [
                                     InkWell(
@@ -57,7 +57,7 @@ class LoanLnameScreen extends StatelessWidget {
                                     Text(
                                       "Give us your Last Name",
                                       style: AppStyle.DmSansFont.copyWith(
-                                          color: ColorConstant.darkBlue,
+                                          color: ColorConstant.primaryBlack,
                                           fontWeight: FontWeight.w700,
                                           fontSize: getFontSize(20)),
                                     ),
@@ -68,72 +68,101 @@ class LoanLnameScreen extends StatelessWidget {
                                       child: Icon(Icons.arrow_back_ios_new_outlined,color: Colors.transparent,),
                                     ),                      ],
                                 ),
-                                SizedBox(
-                                  height: getVerticalSize(57),
-                                ),
-                                Center(
-                                  child: SvgPicture.asset(
-                                    "asset/icons/splash_image.svg",
+                              ),
+                              SizedBox(
+                                height: getVerticalSize(120),
+                              ),
 
-                                    fit: BoxFit.fill,
+
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.only(
+                                        topRight: Radius.circular(20),
+                                        topLeft: Radius.circular(20),
+                                      ),
+                                      color:  ColorConstant.primaryWhite
+                                  ),
+
+                                  child: Padding(
+                                    padding:  EdgeInsets.symmetric(horizontal: getHorizontalSize(20)),
+                                    child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        SizedBox(
+                                          height: getVerticalSize(120),
+                                        ),
+
+                                        Text(
+                                          "To apply, we need your last name linked to \nyour app",
+                                          style: AppStyle.DmSansFont.copyWith(
+                                              color: ColorConstant.grey8F,
+                                              fontWeight: FontWeight.w400,
+                                              fontSize: getFontSize(18)),
+                                        ),
+                                        SizedBox(
+                                          height: getVerticalSize(54),
+                                        ),
+                                        SizedBox(
+                                            child: Obx(()
+                                            => AppTextField(
+                                              controller: loanCalculatorController.lastNameController,
+                                              keyBordType: TextInputType.text,
+                                              hintText: "Last Name",
+
+                                              readOnly:loanCalculatorController.isedit.value? false:true,
+                                              suffixIcon: IconButton(
+                                                icon: Icon(Icons.edit,
+                                                    color:ColorConstant.grey8F,
+                                                    size: getVerticalSize(20)),
+                                                onPressed: (){
+                                                  loanCalculatorController.editText();
+                                                },
+                                              ),
+                                            ),
+                                            )),
+                                        SizedBox(
+                                          height: getVerticalSize(54),
+                                        ),
+                                        AppElevatedButton(
+                                            buttonName: 'Next',
+                                            radius: 5,
+                                            onPressed: () {
+                                              loanCalculatorController.onClickOfLname();
+                                            }),
+
+                                        SizedBox(
+                                          height: getVerticalSize(36),
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
-                                SizedBox(
-                                  height: getVerticalSize(40),
-                                ),
+                              ),
 
-                                SizedBox(
-                                  height: getVerticalSize(5),
-                                ),
-                                Text(
-                                  "To apply, we need your last name linked to \nyour app",
-                                  style: AppStyle.DmSansFont.copyWith(
-                                      color: ColorConstant.grey8F,
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: getFontSize(18)),
-                                ),
-                                SizedBox(
-                                  height: getVerticalSize(54),
-                                ),
-                                SizedBox(
-                                    child: Obx(()
-                                    => AppTextField(
-                                      controller: loanCalculatorController.lastNameController,
-                                      keyBordType: TextInputType.text,
-                                      hintText: "Last Name",
+                            ],
+                          ),
 
-                                      readOnly:loanCalculatorController.isedit.value? false:true,
-                                      suffixIcon: IconButton(
-                                        icon: Icon(Icons.edit,
-                                            color:ColorConstant.grey8F,
-                                            size: getVerticalSize(20)),
-                                        onPressed: (){
-                                          loanCalculatorController.editText();
-                                        },
-                                      ),
-                                    ),
-                                    )),
-                                SizedBox(
-                                  height: getVerticalSize(54),
-                                ),
-                                AppElevatedButton(
-                                    buttonName: 'Next',
-                                    radius: 5,
-                                    onPressed: () {
-                                      loanCalculatorController.onClickOfLname();
-                                    }),
 
-                                SizedBox(
-                                  height: getVerticalSize(36),
-                                ),
-                              ],
+                          Positioned(right: 10,
+                            top: getVerticalSize(60),
+                            child:Center(
+                                child: ClipRRect(
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(100)),
+                                  child: Image.asset('asset/icons/employement_image.jpg',
+                                    height: getVerticalSize(220),),
+                                )
                             ),
                           ),
                         ],
                       ),
                     ),
-                  ],
-                ))));
+                  )
+                ],
+              )),
+        ));
+
 
 
 
--- lib/presentation/LoanCalculator/loan_ssn_screen.dart
+++ lib/presentation/LoanCalculator/loan_ssn_screen.dart
@@ -17,87 +17,83 @@ class LoanSsnScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: ColorConstant.backgroundColor,
         body: SingleChildScrollView(
-            child: Container(
-                height: size.height,
-                child: Stack(
-                  children: [
-                    Image.asset('asset/icons/background_image.png',
-                        fit: BoxFit.cover, width: double.infinity),
-                    SafeArea(
+          child: Container(
+              height: size.height,
+              color: ColorConstant.buttonGreen.withOpacity(0.3),
+              child: Stack(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: getVerticalSize(20)),
+                    child: SafeArea(
                       child: Stack(
                         children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                          Column( crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: getHorizontalSize(20)),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                padding:  EdgeInsets.symmetric(horizontal: getHorizontalSize(20)),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                   children: [
-                                    SizedBox(
-                                      height: getVerticalSize(10),
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        InkWell(
-                                          onTap: () {
-                                            Get.back();
-                                          },
-                                          child: Container(
-                                            decoration: BoxDecoration(
-                                                borderRadius:
-                                                    BorderRadius.circular(12),
-                                                border: Border.all(
-                                                    color: ColorConstant
-                                                        .primaryWhite
-                                                        .withOpacity(0.3))),
-                                            padding: EdgeInsets.all(6),
-                                            child: Icon(
-                                              Icons.arrow_back_ios_new_outlined,
-                                              color: ColorConstant.primaryWhite,
-                                              size: 22,
-                                            ),
-                                          ),
-                                        ),
-                                        Text(
-                                          "Give us your SSN",
-                                          style: AppStyle.DmSansFont.copyWith(
-                                              color: ColorConstant.primaryWhite,
-                                              fontWeight: FontWeight.w700,
-                                              fontSize: getFontSize(20)),
-                                        ),
-                                        Container(
-                                          decoration: BoxDecoration(
-                                              borderRadius:
-                                                  BorderRadius.circular(12),
-                                              border: Border.all(
-                                                  color: Colors.transparent)),
-                                          padding: EdgeInsets.all(10),
-                                          child: Icon(
-                                            Icons.arrow_back_ios_new_outlined,
-                                            color: Colors.transparent,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      height: getVerticalSize(57),
-                                    ),
-                                    Center(
-                                      child: SvgPicture.asset(
-                                        "asset/icons/splash_image.svg",
-                                        fit: BoxFit.fill,
+                                    InkWell(
+                                      onTap: () {
+                                        Get.back();
+                                      },
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                            BorderRadius.circular(12),
+                                            border: Border.all(
+                                                color:
+                                                ColorConstant.backBorder)),
+                                        padding: EdgeInsets.all(6),
+                                        child: Icon(
+                                          Icons.arrow_back_ios_new_outlined,size: 22,),
                                       ),
                                     ),
-                                    SizedBox(
-                                      height: getVerticalSize(40),
+                                    Text(
+                                      "Give us your SSN",
+                                      style: AppStyle.DmSansFont.copyWith(
+                                          color: ColorConstant.primaryBlack,
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: getFontSize(20)),
                                     ),
-                                    SizedBox(
-                                      child: Obx(() => AppTextField(
+                                    Container(
+                                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
+                                          border: Border.all(color: Colors.transparent)),
+                                      padding: EdgeInsets.all(10),
+                                      child: Icon(Icons.arrow_back_ios_new_outlined,color: Colors.transparent,),
+                                    ),                      ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: getVerticalSize(120),
+                              ),
+
+
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.only(
+                                        topRight: Radius.circular(20),
+                                        topLeft: Radius.circular(20),
+                                      ),
+                                      color:  ColorConstant.primaryWhite
+                                  ),
+
+                                  child: Padding(
+                                    padding:  EdgeInsets.symmetric(horizontal: getHorizontalSize(20)),
+                                    child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        SizedBox(
+                                          height: getVerticalSize(120),
+                                        ),
+
+                                        SizedBox(
+                                          child: Obx(() => AppTextField(
                                             controller: loanCalculatorController
                                                 .ssnController,
                                             keyBordType: TextInputType.text,
@@ -106,7 +102,7 @@ class LoanSsnScreen extends StatelessWidget {
                                                   9),
                                             ],
                                             readOnly: loanCalculatorController
-                                                    .isedit.value
+                                                .isedit.value
                                                 ? true
                                                 : false,
                                             hintText: 'SNN',
@@ -120,68 +116,73 @@ class LoanSsnScreen extends StatelessWidget {
                                               },
                                             ),
                                           )),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Spacer(),
-                              Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: getHorizontalSize(20)),
-                                child: AppElevatedButton(
-                                  buttonName: 'Next',
-                                  onPressed: () {
-                                    loanCalculatorController.onClickOfSsn();
-                                    /* debugPrint('Your code: ${enterPersonalDetailController.dobController.text}');
+                                        ),
+                                        Spacer(),
+                                        AppElevatedButton(
+                                          buttonName: 'Next',
+                                          onPressed: () {
+                                            loanCalculatorController.onClickOfSsn();
+                                            /* debugPrint('Your code: ${enterPersonalDetailController.dobController.text}');
                                     enterPersonalDetailController.onTapOfNextButton();*/
-                                  },
+                                          },
+                                        ),
+                                        Spacer(),
+                                        Column(
+                                          children: [
+                                            NumPad(
+                                              type: 'SNN',
+                                              controller: loanCalculatorController.dobController,
+                                              delete: () {
+                                                HapticFeedback.lightImpact();
+                                                if(loanCalculatorController.isedit.value){
+                                                  if( loanCalculatorController.dobController.text.isNotEmpty){
+                                                    loanCalculatorController.dobController.text = loanCalculatorController.dobController.text
+                                                        .substring(0, loanCalculatorController.dobController.text.length - 1);
+
+                                                  }
+                                                }
+
+                                              },
+                                              // do something with the input numbers
+                                              onSubmit: () {
+                                                if(loanCalculatorController.isedit.value){
+                                                  debugPrint('Your code: ${loanCalculatorController.dobController.text}');
+                                                  loanCalculatorController.onClickOfDob();
+                                                }
+
+                                              },
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
                                 ),
                               ),
-                              Spacer(),
-                              Column(
-                                children: [
-                                  NumPad(
-                                    type: 'SNN',
-                                    controller:
-                                        loanCalculatorController.ssnController,
-                                    delete: () {
-                                      HapticFeedback.lightImpact();
-                                      if (loanCalculatorController
-                                          .isedit.value) {
-                                        if (loanCalculatorController
-                                            .ssnController.text.isNotEmpty) {
-                                          loanCalculatorController
-                                                  .ssnController.text =
-                                              loanCalculatorController
-                                                  .ssnController.text
-                                                  .substring(
-                                                      0,
-                                                      loanCalculatorController
-                                                              .ssnController
-                                                              .text
-                                                              .length -
-                                                          1);
-                                        }
-                                      }
-                                    },
-                                    // do something with the input numbers
-                                    onSubmit: () {
-                                      if (loanCalculatorController
-                                          .isedit.value) {
-                                        debugPrint(
-                                            'Your code: ${loanCalculatorController.ssnController.text}');
-                                        loanCalculatorController.onClickOfSsn();
-                                      }
-                                    },
-                                  ),
-                                ],
-                              ),
+
                             ],
+                          ),
+
+
+                          Positioned(right: 10,
+                            top: getVerticalSize(60),
+                            child:Center(
+                                child: ClipRRect(
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(100)),
+                                  child: Image.asset('asset/icons/birthDate_image.png',
+                                    height: getVerticalSize(220),),
+                                )
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ],
-                ))));
+                  )
+                ],
+              )),
+        ));
+
+
   }
 }
